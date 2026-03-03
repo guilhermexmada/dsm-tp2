2467309abf5a70a9c9cbd8b1c9b728be5f108648
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-03-02 22:52:52 -0300

    exercícios 9 e 10 da aula 3 loops

---
+++ aula-03-comandos-repeticao/ex09decresce500.py
@@ -0,0 +1,7 @@
+import os
+os.system('cls')
+
+n = 500
+for i in range(1, 251):
+    print(n)
+    n -= 2
---
+++ aula-03-comandos-repeticao/ex10somaAte800.py
@@ -0,0 +1,10 @@
+import os
+os.system('cls')
+
+i = 0
+total = 0
+
+while i < 800:
+    total += i
+    i += 1
+print(f"A soma da sequência de 1 a 800 = {total}")
